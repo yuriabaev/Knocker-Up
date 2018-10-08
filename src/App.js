7cd6bae21d18219c62522ea7cf74a431590b68d6
Yuri Abaev <yuri.abaev@zooz.com>
2018-10-08 19:16:20 +0300

    adding new

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css'
 import TasksList from './components/TasksList/TasksList'
 import taskStore from './stores/tasks'
 import applicationStore from './stores/application'
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import { TaskCard } from './components/TaskCard/TaskCard'
 //import { configure } from 'mobx';
 
 // configure({
 //   enforceActions: 'strict'
 // });
 //TODO put a provider here
+
 class App extends Component {
+  handleClose = () => {
+    this.props.onClose(this.props.selectedValue);
+  };
+
+  handleListItemClick = value => {
+    this.props.onClose(value);
+  };
+
   render () {
     return (
       <div className="App">
@@ -18,7 +32,28 @@ class App extends Component {
         </header>
         <div className={'page-wrapper'}>
           <TasksList taskStore={taskStore} applicationStore={applicationStore}/>
+          <Button variant="fab" color="primary" aria-label="Add" className={'add-btn'}>
+            <Icon >add_icon</Icon>
+          </Button>
         </div>
+
+        <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title"           open={true}
+        >
+          <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+          <TaskCard key={'taskStore.tasks[0].id'}
+                    id={taskStore.tasks[0].id}
+                    taskName={taskStore.tasks[0].taskName}
+                    description={taskStore.tasks[0].description}
+                    notifyDuration={taskStore.tasks[0].notifyDuration}
+                    recurringDuration={taskStore.tasks[0].recurring}
+                    lastDone={taskStore.tasks[0].lastDone}
+                    daysLeft={taskStore.tasks[0].daysLeft}
+                    dueDate={taskStore.tasks[0].dueDate}
+                    isActive={taskStore.tasks[0].isActive}
+                    isInEditMode={true}
+/>
+        </Dialog>
+
       </div>
     )
   }
